667f3d7f0a91a320ce5c83d5ec9d05c9056a87db
xxRonaldxx <ronald.l.gatewood@gmail.com>
2014-03-29 01:40:02 -0400

    Added username stuff - ron

--- client/client.js
+++ client/client.js
@@ -9,6 +9,14 @@ Template.input.events = {
   'keydown input#message' : function (event) {
     if (event.which == 13) { // 13 is the enter key event
       var name = 'Anonymous';
+      var user = Meteor.user();
+      if (user)
+      {
+        name = Meteor.user().profile.name;
+        if(user.services.github.username){
+           name = user.services.github.username;
+        }
+      }
       var message = document.getElementById('message');
 
       if (message.value != '') {
